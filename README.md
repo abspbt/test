321654972b60f3dca0d44c3820357ada3a8c96af
abspbt <drum3126@gmail.com>
2026-08-19 11:55:08 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pwa-starter-template
+我的極簡 PWA 與 AI 開發標準版型
---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = ['/', '/index.html', '/manifest.json'];
+
+// 安裝並強制等待接管
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting(); 
+});
+
+// 啟動並清除舊快取
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// 攔截網路請求，優先使用快取
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
